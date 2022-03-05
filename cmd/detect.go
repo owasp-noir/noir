fb85ddaac49984962b4b8fa7590558633fd47c35
hahwul <hahwul@gmail.com>
2022-03-06 02:40:56 +0900

    Update

--- cmd/detect.go
+++ cmd/detect.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
-	volt "github.com/hahwul/volt/logger"
+	"fmt"
+	"os"
+
+	file "github.com/hahwul/volt/file"
+	vLog "github.com/hahwul/volt/logger"
 	"github.com/spf13/cobra"
 )
 
@@ -14,8 +18,19 @@ var detectCmd = &cobra.Command{
 	Short: "Detect API and page",
 	Long:  `Detect API and web page in the source code`,
 	Run: func(cmd *cobra.Command, args []string) {
-		logger := volt.GetLogger(debug)
+		logger := vLog.GetLogger(debug)
 		logger.Info("start detect mode")
+		logger.Info("arguments - baseHost: " + baseHost)
+		logger.Info("arguments - basePath: " + basePath)
+		logger.Debug("arguments - output: " + output)
+		logger.Debug("arguments - format: " + format)
+		files, err := file.GetFiles(args[0])
+		if err != nil {
+			logger.Fatal(err)
+		}
+		for _, file := range files {
+			logger.Debug(file)
+		}
 	},
 }
 
@@ -28,4 +43,31 @@ func init() {
 	detectCmd.PersistentFlags().StringVar(&baseHost, "base-host", "http://localhost:80", "base host")
 	detectCmd.PersistentFlags().StringVar(&basePath, "base-path", "/", "base path")
 
+}
+
+func readDir(path string) ([]string, error) {
+	var fileList []string
+	file, err := os.Open(path)
+	if err != nil {
+		return fileList, err
+	}
+	defer file.Close()
+	names, _ := file.Readdirnames(0)
+	for _, name := range names {
+		filePath := fmt.Sprintf("%v/%v", path, name)
+		file, err := os.Open(filePath)
+		if err != nil {
+			return fileList, err
+		}
+		defer file.Close()
+		fileInfo, err := file.Stat()
+		if err != nil {
+			return fileList, err
+		}
+		fileList = append(fileList, filePath)
+		if fileInfo.IsDir() {
+			readDir(filePath)
+		}
+	}
+	return fileList, nil
 }
