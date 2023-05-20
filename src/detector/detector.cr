a29ceeeb75051e56c24539f4b3491e90c383c353
hahwul <hahwul@gmail.com>
2023-05-21 01:49:21 +0900

    Update

---
+++ src/detector/detector.cr
@@ -0,0 +1,11 @@
+def detect(base_path : String)
+    Dir.glob("#{base_path}/**/*") do |file|
+        next if File.directory?(file)
+
+        File.open(file, "r") do |f|
+            f.each_line do |line|
+                # TODO
+            end
+        end
+    end
+end
