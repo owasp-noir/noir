baefff14bd7ea08bcdc0495c18ec19923d57b3b2
hahwul <hahwul@gmail.com>
2023-05-21 01:01:31 +0900

    Add flags

--- src/cmd/cmd.cr
+++ src/cmd/cmd.cr
@@ -2,6 +2,15 @@ require "option_parser"
 
 OptionParser.parse do |parser|
     parser.banner = "Usage: noir <path> <flags>"
+    parser.on "-u", "--url", "Set base URL" do
+      # TODO
+    end
+    parser.on "-o", "--output", "Write result to file" do
+      # TODO
+    end
+    parser.on "-f", "--format", "Set output format [plain/json]" do
+      # TODO
+    end
     parser.on "-v", "--version", "Show version" do
       puts Noir::VERSION
       exit
