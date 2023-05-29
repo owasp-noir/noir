3a88ee1f00d0e84f280ddc1969e3c917e23b8d8e
hahwul <hahwul@gmail.com>
2023-05-30 00:59:19 +0900

    Add go echo

--- src/analysis/analysis.cr
+++ src/analysis/analysis.cr
@@ -5,6 +5,7 @@ def initialize_analyzers
   analyzers["ruby_rails"] = ->analyzer_rails(Hash(Symbol, String))
   analyzers["java_spring"] = ->analyzer_spring(Hash(Symbol, String))
   analyzers["php_pure"] = ->analyzer_php_pure(Hash(Symbol, String))
+  analyzers["go_echo"] = ->analyzer_go_echo(Hash(Symbol, String))
 
   analyzers
 end
---
+++ src/analysis/analyzer/anlyzer_go_echo.cr
@@ -0,0 +1,38 @@
+def analyzer_go_echo(options : Hash(Symbol, String))
+  result = [] of Endpoint
+  base_path = options[:base]
+  url = options[:url]
+
+  # Source Analysis
+  Dir.glob("#{base_path}/**/*") do |path|
+    next if File.directory?(path)
+    if File.exists?(path)
+      File.open(path, "r") do |file|
+        file.each_line do |line|
+            if line.includes?(".GET(") || line.includes?(".POST(") || line.includes?(".PUT(") || line.includes?(".DELETE(")
+              get_route_path_go_echo(line).tap do |route_path|
+                if route_path.size > 0
+                  result << Endpoint.new("#{url}#{route_path}", line.split(".")[1].split("(")[0])
+                end
+              end
+            end
+        end
+      end
+    end
+  end
+
+  result
+end
+
+def get_route_path_go_echo(line : String) : String
+  first = line.strip.split("(")
+  if first.size > 1
+    second = first[1].split(",")
+    if second.size > 1
+      route_path = second[0].gsub("\"", "")
+      return route_path
+    end
+  end
+
+  ""
+end
