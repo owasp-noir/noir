6bb7acd8f7115ffcca8ae1ed91b2450c9a4d57a0
hahwul <hahwul@gmail.com>
2023-06-02 23:21:44 +0900

    Detector speed up

--- src/detector/detector.cr
+++ src/detector/detector.cr
@@ -21,33 +21,36 @@ end
 def detect_relative(base_path : String)
   techs = [] of String
   Dir.glob("#{base_path}/**/*") do |file|
-    next if File.directory?(file)
+    spawn do
+      next if File.directory?(file)
 
-    content = File.read(file)
-    if detect_ruby_rails(file, content)
-      techs << "ruby_rails"
+      content = File.read(file)
+      if detect_ruby_rails(file, content)
+        techs << "ruby_rails"
+      end
+      if detect_ruby_sinatra(file, content)
+        techs << "ruby_sinatra"
+      end
+      if detect_go_echo(file, content)
+        techs << "go_echo"
+      end
+      if detect_java_spring(file, content)
+        techs << "java_spring"
+      end
+      if detect_python_django(file, content)
+        techs << "python_django"
+      end
+      if detect_python_flask(file, content)
+        techs << "python_flask"
+      end
+      if detect_php_pure(file, content)
+        techs << "php_pure"
+      end
+      if detect_java_jsp(file, content)
+        techs << "java_jsp"
+      end
     end
-    if detect_ruby_sinatra(file, content)
-      techs << "ruby_sinatra"
-    end
-    if detect_go_echo(file, content)
-      techs << "go_echo"
-    end
-    if detect_java_spring(file, content)
-      techs << "java_spring"
-    end
-    if detect_python_django(file, content)
-      techs << "python_django"
-    end
-    if detect_python_flask(file, content)
-      techs << "python_flask"
-    end
-    if detect_php_pure(file, content)
-      techs << "php_pure"
-    end
-    if detect_java_jsp(file, content)
-      techs << "java_jsp"
-    end
+    Fiber.yield
   end
 
   techs
