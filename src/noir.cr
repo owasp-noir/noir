4077c073e3be69191905c0b0c3c86b7ba68db0ea
hahwul <hahwul@gmail.com>
2023-06-06 02:03:33 +0900

    Add cli banner

--- src/noir.cr
+++ src/noir.cr
@@ -1,5 +1,7 @@
 require "option_parser"
+require "colorize"
 require "./models/noir.cr"
+require "./logger/banner/banner.cr"
 
 module Noir
   VERSION = "0.1.0"
@@ -13,20 +15,24 @@ module Noir
     "php_pure",
     "java_jsp",
   ]
+
 end
 
 noir_options = {:base => "", :url => "", :format => "plain", :output => "", :techs => ""}
+banner()
+
 OptionParser.parse do |parser|
   parser.banner = "Usage: noir <flags>"
-  parser.separator "  Basic:"
+  parser.separator "  Basic:".colorize(:blue)
   parser.on "-b PATH", "--base-path ./app", "(Required) Set base path" { |var| noir_options[:base] = var }
   parser.on "-u URL", "--url http://..", "Set base url for endpoints" { |var| noir_options[:url] = var }
 
-  parser.separator "\n  Output:"
+  parser.separator "\n  Output:".colorize(:blue)
+
   parser.on "-f FORMAT", "--format json", "Set output format [plain/json/curl/httpie]" { |var| noir_options[:format] = var }
   parser.on "-o PATH", "--output out.txt", "Write result to file" { |var| noir_options[:output] = var }
 
-  parser.separator "\n  Technologies:"
+  parser.separator "\n  Technologies:".colorize(:blue)
   parser.on "-t TECHS", "--techs rails,php", "Set technologies to use" { |var| noir_options[:techs] = var }
   parser.on "-tl", "--techs-list", "Show all technologies" do
     puts "Available technologies:"
@@ -34,7 +40,7 @@ OptionParser.parse do |parser|
     exit
   end
 
-  parser.separator "\n  Others:"
+  parser.separator "\n  Others:".colorize(:blue)
   parser.on "-v", "--version", "Show version" do
     puts Noir::VERSION
     exit
