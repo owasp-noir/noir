1a2b3e579083624af72615e85086171247a1d35a
hahwul <hahwul@gmail.com>
2023-06-08 00:42:33 +0900

    Improve detector of spring

--- src/detector/relative/java_spring.cr
+++ src/detector/relative/java_spring.cr
@@ -1,5 +1,5 @@
 def detect_java_spring(filename : String, file_contents : String)
-  if (filename.includes? "pom.xml") && (file_contents.includes? "org.springframework")
+  if ((filename.includes? "pom.xml") || filename.includes? "build.gradle") && (file_contents.includes? "org.springframework")
     true
   else
     false
