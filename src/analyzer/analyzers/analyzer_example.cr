dad55f6a487e4e11306a49742d4d83e644983af1
hahwul <hahwul@gmail.com>
2023-07-05 01:14:08 +0900

    Update example code for Analyzer

--- src/analyzer/analyzers/analyzer_example.cr
+++ src/analyzer/analyzers/analyzer_example.cr
@@ -2,26 +2,22 @@ require "../../models/analyzer"
 
 class AnalyzerExample < Analyzer
   def analyze
-    # TODO
-  end
-end
+    result = [] of Endpoint
+    base_path = @options[:base]
+    url = @options[:url]
+    _ = url
 
-def analyzer_example(options : Hash(Symbol, String))
-  result = [] of Endpoint
-  base_path = options[:base]
-  url = options[:url]
-  _ = url
-
-  # Source Analysis
-  Dir.glob("#{base_path}/**/*") do |path|
-    next if File.directory?(path)
-    if File.exists?(path)
-      File.open(path, "r") do |file|
-        file.each_line do |_|
+    # Source Analysis
+    Dir.glob("#{base_path}/**/*") do |path|
+      next if File.directory?(path)
+      if File.exists?(path)
+        File.open(path, "r") do |file|
+          file.each_line do |_|
+          end
         end
       end
     end
-  end
 
-  result
+    result
+  end
 end
