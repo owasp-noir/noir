c496a3d0dcaba9fdc7c3e0f6ad68a7fbac6ff92c
hahwul <hahwul@gmail.com>
2023-07-09 16:10:49 +0900

    Update testcode

--- spec/analyzer/analyzer_spring_spec.cr
+++ spec/analyzer/analyzer_spring_spec.cr
@@ -1,10 +1,14 @@
 require "../../src/analyzer/analyzers/analyzer_spring.cr"
+require "../../src/options"
 
 describe "mapping_to_path" do
+  options = default_options()
+  instance = AnalyzerSpring.new(options)
+
   it "mapping_to_path - GET" do
-    mapping_to_path("@GetMapping(\"/abcd\")").should eq("/abcd")
+    instance.mapping_to_path("@GetMapping(\"/abcd\")").should eq("/abcd")
   end
   it "mapping_to_path - POST" do
-    mapping_to_path("@PostMapping(\"/abcd\")").should eq("/abcd")
+    instance.mapping_to_path("@PostMapping(\"/abcd\")").should eq("/abcd")
   end
 end
