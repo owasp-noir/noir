cf544195bcb5cabf97c8da6ec4ecd2e8f656cd75
hahwul <hahwul@gmail.com>
2023-07-14 00:30:22 +0900

    Add query/form/json from detected params in send_proxy

--- src/deliver/send_proxy.cr
+++ src/deliver/send_proxy.cr
@@ -4,14 +4,38 @@ def send_with_proxy(endpoints : Array(Endpoint), proxy : String)
   proxy_url = URI.parse(proxy)
   endpoints.each do |endpoint|
     begin
-      Crest::Request.execute(
-        method: get_symbol(endpoint.method),
-        url: endpoint.url,
-        p_addr: proxy_url.host,
-        p_port: proxy_url.port,
-        tls: OpenSSL::SSL::Context::Client.insecure,
-        user_agent: "Noir/#{Noir::VERSION}"
-      )
+      if endpoint.params.size > 0
+        endpoint_hash = endpoint.params_to_hash
+        body = {} of String => String
+        is_json = false
+        if endpoint_hash["json"].size > 0
+          is_json = true
+          body = endpoint_hash["json"]
+        else
+          body = endpoint_hash["form"]
+        end
+
+        Crest::Request.execute(
+          method: get_symbol(endpoint.method),
+          url: endpoint.url,
+          p_addr: proxy_url.host,
+          p_port: proxy_url.port,
+          tls: OpenSSL::SSL::Context::Client.insecure,
+          user_agent: "Noir/#{Noir::VERSION}",
+          params: endpoint_hash["query"],
+          form: body,
+          json: is_json
+        )
+      else
+        Crest::Request.execute(
+          method: get_symbol(endpoint.method),
+          url: endpoint.url,
+          p_addr: proxy_url.host,
+          p_port: proxy_url.port,
+          tls: OpenSSL::SSL::Context::Client.insecure,
+          user_agent: "Noir/#{Noir::VERSION}"
+        )
+      end
     rescue
     end
   end
@@ -23,6 +47,7 @@ def get_symbol(method : String)
     "POST"   => :post,
     "PUT"    => :put,
     "DELETE" => :delete,
+    "PATCH"  => :patch,
   }
 
   symbol[method]
--- src/models/endpoint.cr
+++ src/models/endpoint.cr
@@ -10,6 +10,19 @@ struct Endpoint
 
   def initialize(@url : String, @method : String, @params : Array(Param))
   end
+
+  def params_to_hash
+    params_hash = {} of String => Hash(String, String)
+    params_hash["query"] = {} of String => String
+    params_hash["json"] = {} of String => String
+    params_hash["form"] = {} of String => String
+
+    @params.each do |param|
+      params_hash[param.param_type][param.name] = param.value
+    end
+
+    params_hash
+  end
 end
 
 struct Param
