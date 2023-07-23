192fbe750d5f21a2d8ad6d84a52128ce5a54fb33
hahwul <hahwul@gmail.com>
2023-07-24 00:41:15 +0900

    Improve code style

--- src/detector/detector.cr
+++ src/detector/detector.cr
@@ -1,39 +1,24 @@
 require "./detectors/*"
 
+macro define_detectors(detectors)
+  {% for detector, index in detectors %}
+    if detect_{{detector}}(file, content)
+      techs << "{{detector}}"
+    end
+  {% end %}
+end
+
 def detect_techs(base_path : String)
   techs = [] of String
   Dir.glob("#{base_path}/**/*") do |file|
     spawn do
       next if File.directory?(file)
-
       content = File.read(file)
-      if detect_ruby_rails(file, content)
-        techs << "ruby_rails"
-      end
-      if detect_ruby_sinatra(file, content)
-        techs << "ruby_sinatra"
-      end
-      if detect_go_echo(file, content)
-        techs << "go_echo"
-      end
-      if detect_java_spring(file, content)
-        techs << "java_spring"
-      end
-      if detect_python_django(file, content)
-        techs << "python_django"
-      end
-      if detect_python_flask(file, content)
-        techs << "python_flask"
-      end
-      if detect_php_pure(file, content)
-        techs << "php_pure"
-      end
-      if detect_java_jsp(file, content)
-        techs << "java_jsp"
-      end
-      if detect_js_express(file, content)
-        techs << "js_express"
-      end
+
+      define_detectors([
+        ruby_rails, ruby_sinatra, go_echo, java_spring, 
+        python_django, python_flask, php_pure, java_jsp, js_express
+      ])
     end
     Fiber.yield
   end
