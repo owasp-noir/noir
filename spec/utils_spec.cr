30dd15320bce5aee5ce57bfd983f028fa481427f
hahwul <hahwul@gmail.com>
2023-07-24 19:17:24 +0900

    Improve testcode

--- spec/utils_spec.cr
+++ spec/utils_spec.cr
@@ -8,3 +8,33 @@ describe "remove_start_slash" do
     remove_start_slash("abcd/1234").should eq("abcd/1234")
   end
 end
+
+describe "get_symbol" do
+  it "get" do
+    get_symbol("GET").should eq(:get)
+  end
+  it "post" do
+    get_symbol("POST").should eq(:post)
+  end
+  it "put" do
+    get_symbol("PUT").should eq(:put)
+  end
+  it "delete" do
+    get_symbol("DELETE").should eq(:delete)
+  end
+  it "head" do
+    get_symbol("HEAD").should eq(:head)
+  end
+  it "options" do
+    get_symbol("OPTIONS").should eq(:options)
+  end
+  it "trace" do
+    get_symbol("TRACE").should eq(:trace)
+  end
+  it "connect" do
+    get_symbol("CONNECT").should eq(:connect)
+  end
+  it "patch" do
+    get_symbol("PATCH").should eq(:patch)
+  end
+end
