abffe2fa37b6a2355abaee939f1e2d64bf613f56
hahwul <hahwul@gmail.com>
2023-08-06 23:27:16 +0900

    Support param in crystal-kemal analyzer

--- src/analyzer/analyzers/analyzer_kemal.cr
+++ src/analyzer/analyzers/analyzer_kemal.cr
@@ -4,59 +4,21 @@ class AnalyzerKemal < Analyzer
   def analyze
     # Source Analysis
     Dir.glob("#{@base_path}/**/*") do |path|
-      spawn do
-        next if File.directory?(path)
-        if File.exists?(path) && File.extname(path) == ".cr" && !path.includes?("spec") && !path.includes?("lib")
-          File.open(path, "r") do |file|
-            file.each_line do |line|
-              line.scan(/get\s+['"](.+?)['"]/) do |match|
-                if match.size > 1
-                  @result << Endpoint.new("#{@url}#{match[1]}", "GET")
-                end
-              end
+      next if File.directory?(path)
+      if File.exists?(path) && File.extname(path) == ".cr" && !path.includes?("spec") && !path.includes?("lib")
+        File.open(path, "r") do |file|
+          last_endpoint = Endpoint.new("", "")
+          file.each_line do |line|
+            endpoint = line_to_endpoint(line)
+            if endpoint.method != ""
+              result << endpoint
+              last_endpoint = endpoint
+            end
 
-              line.scan(/post\s+['"](.+?)['"]/) do |match|
-                if match.size > 1
-                  @result << Endpoint.new("#{@url}#{match[1]}", "POST")
-                end
-              end
-
-              line.scan(/put\s+['"](.+?)['"]/) do |match|
-                if match.size > 1
-                  @result << Endpoint.new("#{@url}#{match[1]}", "PUT")
-                end
-              end
-
-              line.scan(/delete\s+['"](.+?)['"]/) do |match|
-                if match.size > 1
-                  @result << Endpoint.new("#{@url}#{match[1]}", "DELETE")
-                end
-              end
-
-              line.scan(/patch\s+['"](.+?)['"]/) do |match|
-                if match.size > 1
-                  @result << Endpoint.new("#{@url}#{match[1]}", "PATCH")
-                end
-              end
-
-              line.scan(/head\s+['"](.+?)['"]/) do |match|
-                if match.size > 1
-                  @result << Endpoint.new("#{@url}#{match[1]}", "HEAD")
-                end
-              end
-
-              line.scan(/options\s+['"](.+?)['"]/) do |match|
-                if match.size > 1
-                  @result << Endpoint.new("#{@url}#{match[1]}", "OPTIONS")
-                end
-              end
-
-              line.scan(/ws\s+['"](.+?)['"]/) do |match|
-                if match.size > 1
-                  endpoint = Endpoint.new("#{@url}#{match[1]}", "GET")
-                  endpoint.set_protocol("ws")
-                  @result << endpoint
-                end
+            param = line_to_param(line)
+            if param.name != ""
+              if last_endpoint.method != ""
+                last_endpoint.push_param(param)
               end
             end
           end
@@ -64,7 +26,80 @@ class AnalyzerKemal < Analyzer
       end
     end
 
-    @result
+    result
+  end
+
+  def line_to_param(content : String) : Param
+    if content.includes? "env.params.query["
+      param = content.split("env.params.query[")[1].split("]")[0].gsub("\"", "").gsub("'", "")
+      return Param.new(param, "", "query")
+    end
+
+    if content.includes? "env.params.json["
+      param = content.split("env.params.json[")[1].split("]")[0].gsub("\"", "").gsub("'", "")
+      return Param.new(param, "", "json")
+    end
+
+    if content.includes? "env.params.body["
+      param = content.split("env.params.body[")[1].split("]")[0].gsub("\"", "").gsub("'", "")
+      return Param.new(param, "", "body")
+    end
+
+    return Param.new("", "", "")
+  end
+
+  def line_to_endpoint(content : String) : Endpoint
+    content.scan(/get\s+['"](.+?)['"]/) do |match|
+      if match.size > 1
+        return Endpoint.new("#{@url}#{match[1]}", "GET")
+      end
+    end
+
+    content.scan(/post\s+['"](.+?)['"]/) do |match|
+      if match.size > 1
+        return Endpoint.new("#{@url}#{match[1]}", "POST")
+      end
+    end
+
+    content.scan(/put\s+['"](.+?)['"]/) do |match|
+      if match.size > 1
+        return Endpoint.new("#{@url}#{match[1]}", "PUT")
+      end
+    end
+
+    content.scan(/delete\s+['"](.+?)['"]/) do |match|
+      if match.size > 1
+        return Endpoint.new("#{@url}#{match[1]}", "DELETE")
+      end
+    end
+
+    content.scan(/patch\s+['"](.+?)['"]/) do |match|
+      if match.size > 1
+        return Endpoint.new("#{@url}#{match[1]}", "PATCH")
+      end
+    end
+
+    content.scan(/head\s+['"](.+?)['"]/) do |match|
+      if match.size > 1
+        return Endpoint.new("#{@url}#{match[1]}", "HEAD")
+      end
+    end
+
+    content.scan(/options\s+['"](.+?)['"]/) do |match|
+      if match.size > 1
+        return Endpoint.new("#{@url}#{match[1]}", "OPTIONS")
+      end
+    end
+
+    content.scan(/ws\s+['"](.+?)['"]/) do |match|
+      if match.size > 1
+        endpoint = Endpoint.new("#{@url}#{match[1]}", "GET")
+        endpoint.set_protocol("ws")
+        return endpoint
+      end
+    end
+
+    return Endpoint.new("", "")
   end
 end
 
