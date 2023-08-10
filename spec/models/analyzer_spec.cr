e21af3ba21d82c848a3db1309adf8eafb19659f1
hahwul <hahwul@gmail.com>
2023-08-11 01:17:37 +0900

    Add analyzer class spec file

---
+++ spec/models/analyzer_spec.cr
@@ -0,0 +1,20 @@
+require "../../src/models/analyzer.cr"
+require "../../src/options.cr"
+
+describe "Initialize" do
+  options = default_options
+  options[:base] = "noir"
+  object = Analyzer.new(options)
+
+  it "getter - url" do
+    object.url.should eq("")
+  end
+
+  it "getter - scope" do
+    object.scope.should eq("url,param")
+  end
+
+  it "initialized - base_path" do
+    object.base_path.should eq("noir")
+  end
+end
