1a3afdb0e3c6c1314f349b47a13d7271c3ac9bd5
hahwul <hahwul@gmail.com>
2023-08-14 01:16:17 +0900

    Fixed typo 🫥

--- spec/detector/detect_crystal_kemal_spec.cr
+++ spec/detector/detect_crystal_kemal_spec.cr
@@ -4,7 +4,7 @@ describe "Detect Crystal Kemal" do
   options = default_options()
   instance = DetectorCrystalKemal.new options
 
-  it "shards.yml" do
+  it "shard.yml" do
     instance.detect("shard.yml", "kemalcr/kemal").should eq(true)
   end
 end
