3cbbf474f1dfae3bb80f37a55fe03f6a894a81ed
hahwul <hahwul@gmail.com>
2023-08-14 01:15:25 +0900

    Improve detector of kemal

--- src/detector/detectors/crystal_kemal.cr
+++ src/detector/detectors/crystal_kemal.cr
@@ -3,7 +3,6 @@ require "../../models/detector"
 class DetectorCrystalKemal < Detector
   def detect(filename : String, file_contents : String) : Bool
     check = file_contents.includes?("kemalcr/kemal")
-    check = check || file_contents.includes?("dependencies")
     check = check && filename.includes?("shard.yml")
 
     check
