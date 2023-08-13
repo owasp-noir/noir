f18fcf8b1d8085280779ae1932c4ad6cfadb8cd4
hahwul <hahwul@gmail.com>
2023-08-14 01:15:32 +0900

    Improve testcode for techs

--- spec/techs/techs_spec.cr
+++ spec/techs/techs_spec.cr
@@ -5,11 +5,24 @@ describe "Similar to tech" do
     NoirTechs.similar_to_tech("rails").should eq "ruby_rails"
   end
 
+  it "basic2" do 
+    NoirTechs.similar_to_tech("ruby-rails").should eq "ruby_rails"
+  end
+
   it "Upper case" do
     NoirTechs.similar_to_tech("Rails").should eq "ruby_rails"
   end
 
   it "False case" do
     NoirTechs.similar_to_tech("Noir").should_not eq "ruby_rails"
+  end
+end
+
+describe "Get Techs" do 
+  techs = NoirTechs.get_techs
+  techs.each do |k, v|
+    it "#{k.to_s} in techs" do
+      v.should_not be_empty
+    end
   end
 end
