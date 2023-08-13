6987f28bbd7f2b1ba8b76a07d36efa8812b92a4c
hahwul <hahwul@gmail.com>
2023-08-13 19:48:56 +0900

    Improve test code for techs

--- spec/techs/techs_spec.cr
+++ spec/techs/techs_spec.cr
@@ -1,7 +1,15 @@
 require "../../src/techs/techs"
 
 describe "Similar to tech" do
-  it "true" do
+  it "basic" do
     NoirTechs.similar_to_tech("rails").should eq "ruby_rails"
+  end
+
+  it "Upper case" do
+    NoirTechs.similar_to_tech("Rails").should eq "ruby_rails"
+  end
+
+  it "False case" do 
+    NoirTechs.similar_to_tech("Noir").should_not eq "ruby_rails"
   end
 end
