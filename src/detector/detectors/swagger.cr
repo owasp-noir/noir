12c70400927a39ec0e73810eca7191d4c5dc2bb6
hahwul <hahwul@gmail.com>
2023-08-15 23:33:24 +0900

    Improve

--- src/detector/detectors/swagger.cr
+++ src/detector/detectors/swagger.cr
@@ -9,7 +9,7 @@ class DetectorSwagger < Detector
     if valid_json? file_contents
       data = JSON.parse(file_contents)
       begin
-        if !data["swagger"].nil?
+        if !data["swagger"].as_s.nil?
           check = true
           locator = CodeLocator.instance
           locator.set("swagger-json", filename)
@@ -19,7 +19,7 @@ class DetectorSwagger < Detector
     elsif valid_yaml? file_contents
       data = YAML.parse(file_contents)
       begin
-        if !data["swagger"].nil?
+        if !data["swagger"].as_s.nil?
           check = true
           locator = CodeLocator.instance
           locator.set("swagger-yaml", filename)
