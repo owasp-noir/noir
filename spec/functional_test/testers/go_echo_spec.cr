0bb7baef017b50dcabd30d2e9d323157b26e0c37
hahwul <hahwul@gmail.com>
2023-08-27 20:04:06 +0900

    Add param handler in go echo analyzer

--- spec/functional_test/testers/go_echo_spec.cr
+++ spec/functional_test/testers/go_echo_spec.cr
@@ -2,11 +2,19 @@ require "../func_spec.cr"
 
 extected_endpoints = [
   Endpoint.new("/", "GET"),
-  Endpoint.new("/pet", "GET"),
+  Endpoint.new("/pet", "GET", [
+    Param.new("query", "", "query")
+  ]),
+  Endpoint.new("/pet", "POST", [
+    Param.new("name", "", "json")
+  ]),
+  Endpoint.new("/pet_form", "POST", [
+    Param.new("name", "", "body")
+  ]),
   Endpoint.new("/public/secret.html", "GET"),
 ]
 
 FunctionalTester.new("fixtures/go_echo/", {
   :techs     => 1,
-  :endpoints => 3,
+  :endpoints => 5,
 }, extected_endpoints).test_all
