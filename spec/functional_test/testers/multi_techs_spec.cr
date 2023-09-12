1f41e006041aa13e3a09e079024948da6949c110
hahwul <hahwul@gmail.com>
2023-09-12 23:24:35 +0900

    Add OAS case

--- spec/functional_test/testers/multi_techs_spec.cr
+++ spec/functional_test/testers/multi_techs_spec.cr
@@ -5,9 +5,13 @@ extected_endpoints = [
   Endpoint.new("/update", "POST"),
   Endpoint.new("/query", "POST", [Param.new("query", "", "form")]),
   Endpoint.new("/socket", "GET"),
+  Endpoint.new("/pets", "GET"),
+  Endpoint.new("/pets", "POST"),
+  Endpoint.new("/pets/{petId}", "GET"),
+  Endpoint.new("/pets/{petId}", "PUT"),
 ]
 
 FunctionalTester.new("fixtures/multi_techs/", {
-  :techs     => 2,
-  :endpoints => 4,
+  :techs     => 3,
+  :endpoints => 8,
 }, extected_endpoints).test_all
