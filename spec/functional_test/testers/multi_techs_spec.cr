763f2fbe3a2ada428e2d83b4f63b73350500621e
hahwul <hahwul@gmail.com>
2023-11-24 18:47:08 +0900

    Update testcodes

--- spec/functional_test/testers/multi_techs_spec.cr
+++ spec/functional_test/testers/multi_techs_spec.cr
@@ -1,14 +1,25 @@
 require "../func_spec.cr"
 
 extected_endpoints = [
-  Endpoint.new("/", "GET", [Param.new("query", "", "query")]),
+  Endpoint.new("/", "GET", [
+    Param.new("query","","query"),
+  ]),
+  Endpoint.new("/pets", "GET", [
+    Param.new("query","","query"),
+    Param.new("sort","","query"),
+  ]),
   Endpoint.new("/update", "POST"),
   Endpoint.new("/query", "POST", [Param.new("query", "", "form")]),
   Endpoint.new("/socket", "GET"),
   Endpoint.new("/pets", "GET"),
-  Endpoint.new("/pets", "POST"),
+  Endpoint.new("/pets", "POST", [
+    Param.new("name","","json"),
+  ]),
   Endpoint.new("/pets/{petId}", "GET"),
-  Endpoint.new("/pets/{petId}", "PUT"),
+  Endpoint.new("/pets/{petId}", "PUT", [
+    Param.new("breed","","json"),
+    Param.new("name","","json"),
+  ]),
 ]
 
 FunctionalTester.new("fixtures/multi_techs/", {
