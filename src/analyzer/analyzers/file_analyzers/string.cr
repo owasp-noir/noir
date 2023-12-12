7233fb63738264bd24d1ee533234f1d6935170b4
HAHWUL <hahwul@gmail.com>
2023-12-13 08:04:34 +0900

    Add string hook

--- src/analyzer/analyzers/file_analyzers/string.cr
+++ src/analyzer/analyzers/file_analyzers/string.cr
@@ -1,15 +1,19 @@
 require "../../../models/analyzer"
 require "../../../models/endpoint"
 
-FileAnalyzer.add_hook(->(path : String, _url : String) : Array(Endpoint) {
+FileAnalyzer.add_hook(->(path : String, url : String) : Array(Endpoint) {
   results = [] of Endpoint
 
   begin
     File.open(path, "r", encoding: "utf-8", invalid: :skip) do |file|
-      file.each_line do |_|
-        # TODO
-        # e.g
-        # results << Endpoint.new("/", "GET",)
+      file.each_line do |line|
+        url_match = line.match(/\A(https?:\/\/[^\s]+)/)
+        if url_match
+          parsed_url = URI.parse(url_match[1])
+          if parsed_url.to_s.includes? url
+            results << Endpoint.new(parsed_url.path, "GET")
+          end
+        end
       end
     end
   rescue
