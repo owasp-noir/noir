6689794cb8be8610c49691e1b887984ec135c906
HAHWUL <hahwul@gmail.com>
2023-12-13 01:30:44 +0900

    Add testcode for FileAnalyzer (#191)

--- spec/unit_test/models/analyzer_spec.cr
+++ spec/unit_test/models/analyzer_spec.cr
@@ -1,7 +1,7 @@
 require "../../../src/models/analyzer.cr"
 require "../../../src/options.cr"
 
-describe "Initialize" do
+describe "Initialize Analyzer" do
   options = default_options
   options[:base] = "noir"
   object = Analyzer.new(options)
@@ -21,5 +21,32 @@ describe "Initialize" do
 
   it "initialized - base_path" do
     object.base_path.should eq("noir")
+  end
+end
+
+describe "Initialize FileAnalyzer" do
+  options = default_options
+  options[:base] = "noir"
+  object = FileAnalyzer.new(options)
+
+  it "getter - url" do
+    object.url.should eq("")
+  end
+
+  it "getter - scope" do
+    object.scope.should eq("url,param")
+  end
+
+  it "getter - result" do
+    empty = [] of Endpoint
+    object.result.should eq(empty)
+  end
+
+  it "initialized - base_path" do
+    object.base_path.should eq("noir")
+  end
+
+  it "getter - hooks_count" do
+    object.hooks_count.should_not eq(0)
   end
 end
