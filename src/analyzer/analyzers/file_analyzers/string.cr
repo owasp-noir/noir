d5cac5ddf8d1eb95253a213e3fd01432f9c478b6
HAHWUL <hahwul@gmail.com>
2023-12-14 01:16:48 +0900

    Improve pattern in String Hook

--- src/analyzer/analyzers/file_analyzers/string.cr
+++ src/analyzer/analyzers/file_analyzers/string.cr
@@ -7,7 +7,7 @@ FileAnalyzer.add_hook(->(path : String, url : String) : Array(Endpoint) {
   begin
     File.open(path, "r", encoding: "utf-8", invalid: :skip) do |file|
       file.each_line do |line|
-        url_match = line.match(/\A(https?:\/\/[^\s]+)/)
+        url_match = line.match(/\b(https?:\/\/[^\s]+)/)
         if url_match
           parsed_url = URI.parse(url_match[1])
           if parsed_url.to_s.includes? url
