31b27a024b9a43c6a4357d4052d026ea7e65e385
HAHWUL <hahwul@gmail.com>
2024-03-02 23:42:18 +0900

    Add tests for lexer mode

--- spec/unit_test/minilexer/golang_spec.cr
+++ spec/unit_test/minilexer/golang_spec.cr
@@ -6,4 +6,13 @@ describe "initialize" do
   it "init" do
     lexer.class.should eq(GolangLexer)
   end
+
+  it "default mode" do
+    lexer.mode.should eq(:normal)
+  end
+
+  it "persistent mode" do
+    lexer.mode = :persistent
+    lexer.mode.should eq(:persistent)
+  end
 end
