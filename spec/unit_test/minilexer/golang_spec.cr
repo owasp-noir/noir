bf827b11930d297dd7dc90213f541a9a316909f1
HAHWUL <hahwul@gmail.com>
2024-03-05 23:39:58 +0900

    Update tokenize logic for GolangLexer

--- spec/unit_test/minilexer/golang_spec.cr
+++ spec/unit_test/minilexer/golang_spec.cr
@@ -16,3 +16,20 @@ describe "initialize" do
     lexer.mode.should eq(:persistent)
   end
 end
+
+describe "tokenize" do
+  lexer = GolangLexer.new
+
+  it "simple" do
+    output = lexer.tokenize("
+      users := rg.Group(\"/users\")
+
+      users.GET(\"/\", func(c *gin.Context) {
+        c.JSON(http.StatusOK, \"users\")
+      })
+    ")
+    output[0].type.should eq(:newline)
+    output[1].type.should eq(:code)
+    output[2].type.should eq(:string)
+  end
+end
--- src/minilexers/golang.cr
+++ src/minilexers/golang.cr
@@ -1,18 +1,47 @@
 require "../models/minilexer/*"
 
 class GolangLexer < MiniLexer
+  @in_double_quote = false
+  @in_single_quote = false
+  @buffer = ""
+  @identifier = :code
+
   def initialize
     super
-    # You must proceed with the init of the MiniLexer class through super.
-
-    # **********************
-    # Add custom initial state
   end
 
   def tokenize_logic(input : String) : Array(Token)
     results = [] of Token
 
-    # results << Token.new(:identifier, "foo", 3)
+    input.each_char_with_index do |char, index|
+      case char.bytes[0]
+      when 34 # double quote
+        if @in_double_quote
+          results << Token.new(:string, @buffer, index)
+          @in_double_quote = false
+          @identifier = :code
+          @buffer = ""
+        else
+          results << Token.new(@identifier, @buffer, index)
+          @in_double_quote = true
+          @identifier = :string
+        end       
+      when 10 # newline
+        if @buffer != ""
+          results << Token.new(@identifier, @buffer, index)
+        end
+        results << Token.new(:newline, "\n", index)
+        @buffer = ""
+        @identifier = :code
+      else
+        @buffer += char
+      end
+
+      if index == input.size - 1
+        results << Token.new(@identifier, @buffer, index)
+      end
+    end
+
     results
   end
 end
