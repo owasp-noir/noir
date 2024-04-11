231f6610de0e4c2052a2be55fb905bd5c0e486f7
Gby56 <gab.marquet@gmail.com>
2024-04-11 12:20:21 +0200

    --wip-- [skip ci]

---
+++ src/detector/detectors/js_restify.cr
@@ -0,0 +1,17 @@
+require "../../models/detector"
+
+class DetectorJsRestify < Detector
+  def detect(filename : String, file_contents : String) : Bool
+    if (filename.includes? ".js") && (file_contents.includes? "require('restify')")
+      true
+    elsif (filename.includes? ".js") && (file_contents.includes? "require(\"restify\")")
+      true
+    else
+      false
+    end
+  end
+
+  def set_name
+    @name = "js_restify"
+  end
+end
