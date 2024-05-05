e467a823513f283ef1b1c0b53d7c9f4249d932d0
HAHWUL <hahwul@gmail.com>
2024-05-05 17:29:20 +0900

    Fixed #293

--- src/output_builder/only-header.cr
+++ src/output_builder/only-header.cr
@@ -4,12 +4,19 @@ require "../models/endpoint"
 class OutputBuilderOnlyHeader < OutputBuilder
   def print(endpoints : Array(Endpoint))
     headers = [] of String
+    cookie = false
     endpoints.each do |endpoint|
       endpoint.params.each do |param|
         if param.param_type == "header"
           headers << param.name
+        elsif param.param_type == "cookie"
+          cookie = true
         end
       end
+    end
+
+    if cookie
+      headers << "Cookie"
     end
 
     headers.uniq.each do |header|
