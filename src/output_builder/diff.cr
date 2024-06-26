abdbc656fb155724f487831f863f4d4e20a79c18
HAHWUL <hahwul@gmail.com>
2024-06-26 23:57:47 +0900

    🧪 Implement endpoint comparison logic in diff analysis for #330

--- src/output_builder/diff.cr
+++ src/output_builder/diff.cr
@@ -6,9 +6,23 @@ require "yaml"
 
 class OutputBuilderDiff < OutputBuilder
   def diff(new_endpoints : Array(Endpoint), old_endpoints : Array(Endpoint))
-    added = new_endpoints - old_endpoints
-    removed = old_endpoints - new_endpoints
-    changed = new_endpoints & old_endpoints
+    added = [] of Endpoint
+    changed = [] of Endpoint
+    removed = [] of Endpoint
+
+    new_endpoints.each do |new_endpoint|
+      matching_old_endpoint = old_endpoints.find { |old_endpoint| old_endpoint.url == new_endpoint.url && old_endpoint.method == new_endpoint.method }
+      if matching_old_endpoint
+        changed << new_endpoint unless new_endpoint == matching_old_endpoint
+      else
+        added << new_endpoint
+      end
+    end
+
+    old_endpoints.each do |old_endpoint|
+      matching_new_endpoint = new_endpoints.find { |new_endpoint| new_endpoint.url == old_endpoint.url && new_endpoint.method == old_endpoint.method }
+      removed << old_endpoint unless matching_new_endpoint
+    end
 
     {added: added, removed: removed, changed: changed}
   end
