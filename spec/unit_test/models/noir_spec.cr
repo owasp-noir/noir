3d8a18b35390dffa05797c07cd9fb2be97cc2d4c
HAHWUL <hahwul@gmail.com>
2024-09-21 01:06:13 +0900

    Add test codes

--- spec/unit_test/models/noir_spec.cr
+++ spec/unit_test/models/noir_spec.cr
@@ -31,3 +31,38 @@ describe "Methods" do
     runner.endpoints[1].url.should eq("https://www.hahwul.com/abcd")
   end
 end
+
+describe "set-pvalue" do
+  config_init = ConfigInitializer.new
+  options = config_init.default_options
+  options["base"] = YAML::Any.new("noir")
+  options["set_pvalue_query"] = YAML::Any.new([YAML::Any.new("FUZZ")])
+  options["set_pvalue_header"] = YAML::Any.new([YAML::Any.new("name=FUZZ")])
+  options["set_pvalue_cookie"] = YAML::Any.new([YAML::Any.new("name:FUZZ")])
+  options["set_pvalue_json"] = YAML::Any.new([YAML::Any.new("name:FUZZ=FUZZ")])
+  runner = NoirRunner.new(options)
+
+  it "applies pvalue to query parameter" do
+    runner.apply_pvalue("query", "name", "value").should eq("FUZZ")
+  end
+  
+  it "applies pvalue to header parameter with '=' delimiter" do
+    runner.apply_pvalue("header", "name", "value").should eq("FUZZ")
+  end
+  
+  it "does not apply pvalue to header parameter when name does not match" do
+    runner.apply_pvalue("header", "name2", "value").should eq("value")
+  end
+  
+  it "applies pvalue to cookie parameter with ':' delimiter" do
+    runner.apply_pvalue("cookie", "name", "value").should eq("FUZZ")
+  end
+  
+  it "does not apply pvalue to cookie parameter when name does not match" do
+    runner.apply_pvalue("cookie", "name2", "value").should eq("value")
+  end
+  
+  it "includes '=' in the pvalue for JSON parameter" do
+    runner.apply_pvalue("json", "name", "value").should eq("FUZZ=FUZZ")
+  end
+end
