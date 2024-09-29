22f2c4c3c092d0d921f337202727c8675330788d
HAHWUL <hahwul@gmail.com>
2024-09-29 23:42:52 +0900

    Refactor severity color in OutputBuilderPassiveScan
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- src/output_builder/passive_scan.cr
+++ src/output_builder/passive_scan.cr
@@ -7,9 +7,26 @@ require "yaml"
 class OutputBuilderPassiveScan < OutputBuilder
   def print(passive_results : Array(PassiveScanResult), logger : NoirLogger, is_color : Bool)
     passive_results.each do |result|
-      logger.puts "[#{result.id.colorize(:light_blue).toggle(is_color)}][#{result.category.colorize(:light_yellow).toggle(is_color)}] #{result.info.name.colorize(:light_green).toggle(is_color)}"
+      logger.puts "[#{severity_color(result.info.severity)}][#{result.id.colorize(:light_blue).toggle(is_color)}][#{result.category.colorize(:light_yellow).toggle(is_color)}] #{result.info.name.colorize(:light_green).toggle(is_color)}"
       logger.sub "├── extract: #{result.extract}"
       logger.sub "└── file: #{result.file_path}:#{result.line_number}"
+    end
+  end
+
+  def severity_color(severity : String) : String
+    case severity
+    when "critical"
+      severity.colorize(:red).to_s
+    when "high"
+      severity.colorize(:light_red).to_s
+    when "medium"
+      severity.colorize(:yellow).to_s
+    when "low"
+      severity.colorize(:light_yellow).to_s
+    when "info"
+      severity.colorize(:light_blue).to_s
+    else
+      severity.colorize(:light_white).to_s
     end
   end
 end
