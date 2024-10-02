b9bdc94332dd07a644d354ee7d06ab1d2dd4c059
HAHWUL <hahwul@gmail.com>
2024-10-02 23:16:52 +0900

    ✨ Add ActixWeb
    Signed-off-by: HAHWUL <hahwul@gmail.com>

---
+++ spec/functional_test/testers/rust_actix_web_spec.cr
@@ -0,0 +1,11 @@
+require "../func_spec.cr"
+
+extected_endpoints = [
+  Endpoint.new("/", "GET"),
+  Endpoint.new("/echo", "POST"),
+]
+
+FunctionalTester.new("fixtures/rust_actix_web/", {
+  :techs     => 1,
+  :endpoints => extected_endpoints.size,
+}, extected_endpoints).test_all
