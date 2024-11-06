ab505730b129d6a205d295bcb66c569b36690a4b
HAHWUL <hahwul@gmail.com>
2024-11-06 23:37:09 +0900

    Add endpoint for rails spec
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- spec/functional_test/fixtures/ruby/rails/config/routes.rb
+++ spec/functional_test/fixtures/ruby/rails/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
     resources :posts
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
     # Defines the root path route ("/")
     # root "articles#index"
+    get "up" => "rails/health#show", as: :rails_health_check
+    get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+    get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   end
