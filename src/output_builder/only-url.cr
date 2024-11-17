ff665d526070366cdfab01534780985c7e353847
HAHWUL <hahwul@gmail.com>
2024-11-17 22:16:20 +0900

    feat: prevent duplicate URL printing in output
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- src/output_builder/only-url.cr
+++ src/output_builder/only-url.cr
@@ -3,10 +3,17 @@ require "../models/endpoint"
 
 class OutputBuilderOnlyUrl < OutputBuilder
   def print(endpoints : Array(Endpoint))
+    printed_urls = Set(String).new
+
     endpoints.each do |endpoint|
       baked = bake_endpoint(endpoint.url, endpoint.params)
-      r_url = baked[:url].colorize(:light_yellow).toggle(@is_color)
-      ob_puts "#{r_url}"
+      plain_url = baked[:url]
+      r_url = plain_url.colorize(:light_yellow).toggle(@is_color)
+
+      unless printed_urls.includes?(plain_url)
+        ob_puts "#{r_url}"
+        printed_urls.add(plain_url)
+      end
     end
   end
 end
