4ddf83e39037514c0e5173769688b3a26d284fcb
HAHWUL <hahwul@gmail.com>
2025-01-12 23:33:16 +0900

    feat(detector): add support for ZAP sites tree specification detection
    Signed-off-by: HAHWUL <hahwul@gmail.com>

---
+++ src/detector/detectors/specification/zap_sites_tree.cr
@@ -0,0 +1,29 @@
+require "../../../models/detector"
+require "../../../utils/json"
+require "../../../utils/yaml"
+require "../../../models/code_locator"
+
+module Detector::Specification
+  class ZapSitesTree < Detector
+    def detect(filename : String, file_contents : String) : Bool
+      check = false
+      if valid_yaml? file_contents
+        data = YAML.parse(file_contents)
+        begin
+          if data[0]["node"].as_s.includes? "Sites"
+            check = true
+            locator = CodeLocator.instance
+            locator.push("zap-sites-tree", filename)
+          end
+        rescue
+        end
+      end
+
+      check
+    end
+
+    def set_name
+      @name = "zap_sites_tree"
+    end
+  end
+end
