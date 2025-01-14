931fa760d7e0e95f90e87485f7f627efba3b86e7
HAHWUL <hahwul@gmail.com>
2025-01-15 00:03:02 +0900

    test(detector): add additional tests for Express detection with various import styles
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- spec/unit_test/detector/javascript/express_spec.cr
+++ spec/unit_test/detector/javascript/express_spec.cr
@@ -8,7 +8,48 @@ describe "Detect JS Express" do
   it "require_single_quot" do
     instance.detect("index.js", "require('express')").should eq(true)
   end
+
   it "require_double_quot" do
     instance.detect("index.js", "require(\"express\")").should eq(true)
+  end
+
+  it "import_single_quot" do
+    instance.detect("index.js", "import express from 'express'").should eq(true)
+  end
+
+  it "import_double_quot" do
+    instance.detect("index.js", "import express from \"express\"").should eq(true)
+  end
+
+  it "const_require_single_quot" do
+    instance.detect("index.js", "const express = require('express')").should eq(true)
+  end
+
+  it "const_require_double_quot" do
+    instance.detect("index.js", "const express = require(\"express\")").should eq(true)
+  end
+
+  it "let_require_single_quot" do
+    instance.detect("index.js", "let express = require('express')").should eq(true)
+  end
+
+  it "let_require_double_quot" do
+    instance.detect("index.js", "let express = require(\"express\")").should eq(true)
+  end
+
+  it "var_require_single_quot" do
+    instance.detect("index.js", "var express = require('express')").should eq(true)
+  end
+
+  it "var_require_double_quot" do
+    instance.detect("index.js", "var express = require(\"express\")").should eq(true)
+  end
+
+  it "import_router_single_quot" do
+    instance.detect("index.js", "import { Router } from 'express'").should eq(true)
+  end
+
+  it "import_router_double_quot" do
+    instance.detect("index.js", "import { Router } from \"express\"").should eq(true)
   end
 end
