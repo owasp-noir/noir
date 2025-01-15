52a67539fce99f7e08f3de1028f7e2f16565d083
HAHWUL <hahwul@gmail.com>
2025-01-15 22:52:28 +0900

    Linting
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- src/detector/detectors/javascript/express.cr
+++ src/detector/detectors/javascript/express.cr
@@ -5,10 +5,10 @@ module Detector::Javascript
     def detect(filename : String, file_contents : String) : Bool
       if (filename.includes?(".js") || filename.includes?(".mjs") || filename.includes?(".ts")) &&
          (file_contents.match(/require\(['"]express['"]\)/) ||
-          file_contents.match(/import express from ['"]express['"]/) ||
-          file_contents.match(/import { Router } from ['"]express['"]/) ||
-          file_contents.match(/app\.use\(express\.json\(\)\)/) ||
-          file_contents.match(/app\.use\(express\.urlencoded\(\{ extended: true \}\)\)/))
+         file_contents.match(/import express from ['"]express['"]/) ||
+         file_contents.match(/import { Router } from ['"]express['"]/) ||
+         file_contents.match(/app\.use\(express\.json\(\)\)/) ||
+         file_contents.match(/app\.use\(express\.urlencoded\(\{ extended: true \}\)\)/))
         true
       else
         false
