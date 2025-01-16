86ea5f9c71d919c373d7371248ae5fb279efbf73
HAHWUL <hahwul@gmail.com>
2025-01-17 00:02:01 +0900

    feat(analyzer): enhance concurrency in Ruby file analysis using Channel and WaitGroup
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- src/analyzer/analyzers/ruby/sinatra.cr
+++ src/analyzer/analyzers/ruby/sinatra.cr
@@ -4,26 +4,47 @@ module Analyzer::Ruby
   class Sinatra < Analyzer
     def analyze
       # Source Analysis
-      begin
-        Dir.glob("#{@base_path}/**/*") do |path|
-          next if File.directory?(path)
-          if File.exists?(path)
-            File.open(path, "r", encoding: "utf-8", invalid: :skip) do |file|
-              last_endpoint = Endpoint.new("", "")
-              file.each_line.with_index do |line, index|
-                endpoint = line_to_endpoint(line)
-                if endpoint.method != ""
-                  details = Details.new(PathInfo.new(path, index + 1))
-                  endpoint.details = details
-                  @result << endpoint
-                  last_endpoint = endpoint
-                end
+      channel = Channel(String).new
 
-                param = line_to_param(line)
-                if param.name != ""
-                  if last_endpoint.method != ""
-                    last_endpoint.push_param(param)
+      begin
+        spawn do
+          Dir.glob("#{@base_path}/**/*") do |file|
+            channel.send(file)
+          end
+          channel.close
+        end
+
+        WaitGroup.wait do |wg|
+          @options["concurrency"].to_s.to_i.times do
+            wg.spawn do
+              loop do
+                begin
+                  path = channel.receive?
+                  break if path.nil?
+                  next if File.directory?(path)
+                  if File.exists?(path)
+                    File.open(path, "r", encoding: "utf-8", invalid: :skip) do |file|
+                      last_endpoint = Endpoint.new("", "")
+                      file.each_line.with_index do |line, index|
+                        endpoint = line_to_endpoint(line)
+                        if endpoint.method != ""
+                          details = Details.new(PathInfo.new(path, index + 1))
+                          endpoint.details = details
+                          @result << endpoint
+                          last_endpoint = endpoint
+                        end
+
+                        param = line_to_param(line)
+                        if param.name != ""
+                          if last_endpoint.method != ""
+                            last_endpoint.push_param(param)
+                          end
+                        end
+                      end
+                    end
                   end
+                rescue e : File::NotFoundError
+                  logger.debug "File not found: #{path}"
                 end
               end
             end
