e91797be6ea6632a3c1e4706bdaf73ad0f4e2625
HAHWUL <hahwul@gmail.com>
2025-01-16 23:46:00 +0900

    feat(analyzer): enhance concurrency in Crystal file analysis using Channel and WaitGroup
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- src/analyzer/analyzers/crystal/lucky.cr
+++ src/analyzer/analyzers/crystal/lucky.cr
@@ -15,27 +15,48 @@ module Analyzer::Crystal
         logger.debug e
       end
 
+      channel = Channel(String).new
+
+      spawn do
+        Dir.glob("#{@base_path}/**/*") do |file|
+          channel.send(file)
+        end
+        channel.close
+      end
+
       # Source Analysis
       begin
-        Dir.glob("#{@base_path}/**/*") do |path|
-          next if File.directory?(path)
-          if File.exists?(path) && File.extname(path) == ".cr" && !path.includes?("lib")
-            File.open(path, "r", encoding: "utf-8", invalid: :skip) do |file|
-              last_endpoint = Endpoint.new("", "")
-              file.each_line.with_index do |line, index|
-                endpoint = line_to_endpoint(line)
-                if endpoint.method != ""
-                  details = Details.new(PathInfo.new(path, index + 1))
-                  endpoint.details = details
-                  result << endpoint
-                  last_endpoint = endpoint
-                end
-
-                param = line_to_param(line)
-                if param.name != ""
-                  if last_endpoint.method != ""
-                    last_endpoint.push_param(param)
+        WaitGroup.wait do |wg|
+          @options["concurrency"].to_s.to_i.times do
+            wg.spawn do
+              loop do
+                begin
+                  path = channel.receive?
+                  break if path.nil?
+                  next if File.directory?(path)
+                  if File.exists?(path) && File.extname(path) == ".cr" && !path.includes?("lib")
+                    File.open(path, "r", encoding: "utf-8", invalid: :skip) do |file|
+                      last_endpoint = Endpoint.new("", "")
+                      file.each_line.with_index do |line, index|
+                        endpoint = line_to_endpoint(line)
+                        if endpoint.method != ""
+                          details = Details.new(PathInfo.new(path, index + 1))
+                          endpoint.details = details
+                          result << endpoint
+                          last_endpoint = endpoint
+                        end
+        
+                        param = line_to_param(line)
+                        if param.name != ""
+                          if last_endpoint.method != ""
+                            last_endpoint.push_param(param)
+                          end
+                        end
+                      end
+                    end
                   end
+                rescue e : File::NotFoundError
+                  logger.debug "File not found: #{path}"
                 end
               end
             end
