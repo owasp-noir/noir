0bff23680fa8112a502c69a188abbd6288350614
HAHWUL <hahwul@gmail.com>
2025-01-16 23:56:57 +0900

    feat(analyzer): enhance concurrency in Java file analysis using Channel and WaitGroup
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- src/analyzer/analyzers/java/armeria.cr
+++ src/analyzer/analyzers/java/armeria.cr
@@ -8,49 +8,70 @@ module Analyzer::Java
 
     def analyze
       # Source Analysis
+      channel = Channel(String).new
+
       begin
-        Dir.glob("#{@base_path}/**/*") do |path|
-          next if File.directory?(path)
+        spawn do
+          Dir.glob("#{@base_path}/**/*") do |file|
+            channel.send(file)
+          end
+          channel.close
+        end
 
-          if File.exists?(path) && (path.ends_with?(".java") || path.ends_with?(".kt"))
-            details = Details.new(PathInfo.new(path))
+        WaitGroup.wait do |wg|
+          @options["concurrency"].to_s.to_i.times do
+            wg.spawn do
+              loop do
+                begin
+                  path = channel.receive?
+                  break if path.nil?
+                  next if File.directory?(path)
 
-            content = File.read(path, encoding: "utf-8", invalid: :skip)
-            content.scan(REGEX_SERVER_CODE_BLOCK) do |server_codeblcok_match|
-              server_codeblock = server_codeblcok_match[0]
+                  if File.exists?(path) && (path.ends_with?(".java") || path.ends_with?(".kt"))
+                    details = Details.new(PathInfo.new(path))
 
-              server_codeblock.scan(REGEX_SERVICE_CODE) do |service_code_match|
-                next if service_code_match.size != 2
-                endpoint_param_index = 0
-                if service_code_match[1] == "If"
-                  endpoint_param_index = 1
+                    content = File.read(path, encoding: "utf-8", invalid: :skip)
+                    content.scan(REGEX_SERVER_CODE_BLOCK) do |server_codeblock_match|
+                      server_codeblock = server_codeblock_match[0]
+
+                      server_codeblock.scan(REGEX_SERVICE_CODE) do |service_code_match|
+                        next if service_code_match.size != 2
+                        endpoint_param_index = 0
+                        if service_code_match[1] == "If"
+                          endpoint_param_index = 1
+                        end
+
+                        service_code = service_code_match[0]
+                        parameter_code = service_code.split("(")[1]
+                        split_params = parameter_code.split(",")
+                        next if split_params.size <= endpoint_param_index
+                        endpoint = split_params[endpoint_param_index].strip
+
+                        endpoint = endpoint[1..-2]
+                        @result << Endpoint.new("#{endpoint}", "GET", details)
+                      end
+
+                      server_codeblock.scan(REGEX_ROUTE_CODE) do |route_code_match|
+                        next if route_code_match.size != 3
+                        method = route_code_match[1].upcase
+                        if method == "PATH"
+                          method = "GET"
+                        end
+
+                        next if !["GET", "POST", "DELETE", "PUT", "PATCH", "HEAD", "OPTIONS"].includes?(method)
+
+                        endpoint = route_code_match[2].split(")")[0].strip
+                        next if endpoint[0] != endpoint[-1]
+                        next if endpoint[0] != '"'
+
+                        endpoint = endpoint[1..-2]
+                        @result << Endpoint.new("#{endpoint}", method, details)
+                      end
+                    end
+                  end
+                rescue e : File::NotFoundError
+                  logger.debug "File not found: #{path}"
                 end
-
-                service_code = service_code_match[0]
-                parameter_code = service_code.split("(")[1]
-                split_params = parameter_code.split(",")
-                next if split_params.size <= endpoint_param_index
-                endpoint = split_params[endpoint_param_index].strip
-
-                endpoint = endpoint[1..-2]
-                @result << Endpoint.new("#{endpoint}", "GET", details)
-              end
-
-              server_codeblock.scan(REGEX_ROUTE_CODE) do |route_code_match|
-                next if route_code_match.size != 3
-                method = route_code_match[1].upcase
-                if method == "PATH"
-                  method = "GET"
-                end
-
-                next if !["GET", "POST", "DELETE", "PUT", "PATCH", "HEAD", "OPTIONS"].includes?(method)
-
-                endpoint = route_code_match[2].split(")")[0].strip
-                next if endpoint[0] != endpoint[-1]
-                next if endpoint[0] != '"'
-
-                endpoint = endpoint[1..-2]
-                @result << Endpoint.new("#{endpoint}", method, details)
               end
             end
           end
