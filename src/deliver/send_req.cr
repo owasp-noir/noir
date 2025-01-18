f100dbb3433bf3614d74361fab122fc1565e704f
HAHWUL <hahwul@gmail.com>
2025-01-18 16:45:42 +0900

    feat(deliver): improve concurrency in send_req by utilizing WaitGroup for endpoint processing
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- src/deliver/send_req.cr
+++ src/deliver/send_req.cr
@@ -1,44 +1,54 @@
 require "crest"
+require "wait_group"
 require "../utils/http_symbols"
 require "../models/deliver"
 
 class SendReq < Deliver
   def run(endpoints : Array(Endpoint))
     applied_endpoints = apply_all(endpoints)
-    applied_endpoints.each do |endpoint|
-      begin
-        if endpoint.params.size > 0
-          endpoint_hash = endpoint.params_to_hash
-          body = {} of String => String
-          is_json = false
-          if endpoint_hash["json"].size > 0
-            is_json = true
-            body = endpoint_hash["json"]
-          else
-            body = endpoint_hash["form"]
-          end
+    wg = WaitGroup.new
 
-          Crest::Request.execute(
-            method: get_symbol(endpoint.method),
-            url: endpoint.url,
-            tls: OpenSSL::SSL::Context::Client.insecure,
-            user_agent: "Noir/#{Noir::VERSION}",
-            params: endpoint_hash["query"],
-            form: body,
-            headers: @headers,
-            json: is_json
-          )
-        else
-          Crest::Request.execute(
-            method: get_symbol(endpoint.method),
-            url: endpoint.url,
-            headers: @headers,
-            tls: OpenSSL::SSL::Context::Client.insecure,
-            user_agent: "Noir/#{Noir::VERSION}"
-          )
+    applied_endpoints.each do |endpoint|
+      wg.add(1)
+      spawn do
+        begin
+          if endpoint.params.size > 0
+            endpoint_hash = endpoint.params_to_hash
+            body = {} of String => String
+            is_json = false
+            if endpoint_hash["json"].size > 0
+              is_json = true
+              body = endpoint_hash["json"]
+            else
+              body = endpoint_hash["form"]
+            end
+
+            Crest::Request.execute(
+              method: get_symbol(endpoint.method),
+              url: endpoint.url,
+              tls: OpenSSL::SSL::Context::Client.insecure,
+              user_agent: "Noir/#{Noir::VERSION}",
+              params: endpoint_hash["query"],
+              form: body,
+              headers: @headers,
+              json: is_json
+            )
+          else
+            Crest::Request.execute(
+              method: get_symbol(endpoint.method),
+              url: endpoint.url,
+              headers: @headers,
+              tls: OpenSSL::SSL::Context::Client.insecure,
+              user_agent: "Noir/#{Noir::VERSION}"
+            )
+          end
+        rescue
+        ensure
+          wg.done
         end
-      rescue
       end
     end
+
+    wg.wait
   end
 end
