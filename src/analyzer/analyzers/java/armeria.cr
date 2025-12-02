9b0387b694a380fa729cabc0900c37af01c49597
HAHWUL <hahwul@gmail.com>
2025-12-02 23:24:01 +0900

    Fix lint
    Signed-off-by: HAHWUL <hahwul@gmail.com>

--- src/analyzer/analyzers/java/armeria.cr
+++ src/analyzer/analyzers/java/armeria.cr
@@ -140,7 +140,7 @@ module Analyzer::Java
         end
 
         # Handle @Get(value = "/path") or named parameters
-        param_tokens.each_with_index do |token, index|
+        param_tokens.each_with_index do |token, _|
           if token.type == :STRING_LITERAL
             value = strip_quotes(token.value)
             return value unless value.empty?
