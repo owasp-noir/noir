b1543cb1f5564f335621c72972d503cbcf5bd2b7
HAHWUL <hahwul@gmail.com>
2025-12-29 17:27:49 +0900

    Quote shard names and add toml dependency

--- shards.nix
+++ shards.nix
@@ -1,22 +1,27 @@
 {
-  crest = {
+  "crest" = {
     url = "https://github.com/mamantoha/crest.git";
     rev = "v1.4.1";
     sha256 = "1dh6bk8zwkg3d7jvz10r0yydznbxhynjyja010bis42qwryx42rl";
   };
-  har = {
+  "har" = {
     url = "https://github.com/neuralegion/har.git";
     rev = "v1.2.0";
     sha256 = "102w2awn7v96p16vqs5zb0633jj4d30jhs02fh5ic9j9qgj1cvhw";
   };
-  http-client-digest_auth = {
+  "http-client-digest_auth" = {
     url = "https://github.com/mamantoha/http-client-digest_auth.git";
     rev = "v0.6.0";
     sha256 = "1qd6k36bfyjx1c448y5s60rfs8n5mf1dc70cbpf1cjwkg2b1cp36";
   };
-  http_proxy = {
+  "http_proxy" = {
     url = "https://github.com/mamantoha/http_proxy.git";
     rev = "v0.13.1";
     sha256 = "0ah895a9hpmflf1hpqfk43kai0qgd51bpws44hb3wihya65n1qya";
+  };
+  "toml" = {
+    url = "https://github.com/crystal-community/toml.cr.git";
+    rev = "v0.8.1";
+    sha256 = "1v1hpx7f772ky09r9fzyx5d1gvw8kih5msvp0h81dg4cpgmy8lv5";
   };
 }
