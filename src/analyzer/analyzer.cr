6cf907af9a1f71aedbe3e042918bc96bbd84d8c1
HAHWUL <hahwul@gmail.com>
2026-05-21 21:09:06 +0900

    Add Python Robyn analyzer (#1708)
    Closes #1640

--- src/analyzer/analyzer.cr
+++ src/analyzer/analyzer.cr
@@ -118,6 +118,7 @@ def initialize_analyzers(logger : NoirLogger)
     {"python_flask", Python::Flask},
     {"python_litestar", Python::Litestar},
     {"python_pyramid", Python::Pyramid},
+    {"python_robyn", Python::Robyn},
     {"python_sanic", Python::Sanic},
     {"python_starlette", Python::Starlette},
     {"python_tornado", Python::Tornado},
