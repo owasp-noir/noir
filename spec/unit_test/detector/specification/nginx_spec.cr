8f10a48ac4b6ddd2dc7d9dc1e416a8b8f6b9d1be
hahwul <hahwul@gmail.com>
2026-06-06 19:36:23 +0900

    Improve nginx spec detection

--- spec/unit_test/detector/specification/nginx_spec.cr
+++ spec/unit_test/detector/specification/nginx_spec.cr
@@ -26,7 +26,36 @@ describe "Detect Nginx config" do
     instance.detect("sites-enabled/api.conf", src).should be_true
   end
 
+  it "detects nginx include fragments without server blocks" do
+    instance.detect("h5bp/location/security_file_access.conf", <<-CONF).should be_true
+      location ~* /\\.(?!well-known\\/) {
+          deny all;
+      }
+      CONF
+  end
+
+  it "detects nginx templates" do
+    instance.detect("nginx.tmpl", <<-CONF).should be_true
+      {{ if .Enabled }}
+      server {
+          location /debug {
+              return 200;
+          }
+      }
+      {{ end }}
+      CONF
+  end
+
   it "rejects .conf without nginx-shape directives" do
     instance.detect("config.conf", "key = value\nfoo = bar\n").should be_false
+  end
+
+  it "rejects nginx-looking words inside comments" do
+    instance.detect("comment.conf", <<-CONF).should be_false
+      # server {
+      #   location /commented-out {
+      #   }
+      # }
+      CONF
   end
 end
